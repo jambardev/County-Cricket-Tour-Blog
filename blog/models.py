15177cf80f42934e6d08c069b1df77df1894f9fd
James Barlow <jamesbarlow1@hotmail.co.uk>
2024-07-03 15:15:52 +0000

    Additional fields added to post model

--- blog/models.py
+++ blog/models.py
@@ -2,6 +2,53 @@ from django.db import models
 from django.contrib.auth.models import User
 
 STATUS = ((0, "Draft"), (1, "Published"))
+STADIUM_CHOICES = [
+    ('racecourse', 'Racecourse Ground (Derbyshire)'),
+    ('riverside', 'Riverside (Durham)'),
+    ('chelmsford', 'Chelmsford (Essex)'),
+    ('sophia gardens', 'Sophia Gardens (Glamorgan)'),
+    ('nevil road', 'Nevil Road (Gloucestershire)'),
+    ('ageas bowl', 'Ageas Bowl (Hampshire)'),
+    ('canterbury', 'Canterbury (Kent)'),
+    ('old trafford', 'Old Trafford (Lancashire)'),
+    ('grace road', 'Grace Road (Leicestershire)'),
+    ('lords', 'Lords (Middlesex)'),
+    ('northampton', 'Northampton (Northamptonsire)'),
+    ('trent bridge', 'Trent Bridge (Nottinghamshire)'),
+    ('taunton', 'Taunton (Somerset)'),
+    ('oval', 'The Oval (Surrey)'),
+    ('hove', 'Hove (Sussex)'),
+    ('edgbaston', 'Edgbaston (Warwickshire)'),
+    ('new road', 'New Road (Worcestershire)'),
+    ('headingley', 'Headingley (Yorkshire)'),
+]
+VISITORS_CHOICES = [
+    ('derbyshire', 'Derbyshire'),
+    ('durham', 'Durham'),
+    ('essex', 'Essex'),
+    ('glamorgan', 'Glamorgan'),
+    ('gloucestershire', 'Gloucestershire'),
+    ('hampshire', 'Hampshire'),
+    ('kent', 'Kent'),
+    ('lancashire', 'Lancashire'),
+    ('leicestershire', 'Leicestershire'),
+    ('middlesex', 'Middlesex'),
+    ('northamptonshire', 'Northamptonshire'),
+    ('nottinghamshire', 'Nottinghamshire'),
+    ('somerset', 'Somerset'),
+    ('surrey', 'Surrey'),
+    ('sussex', 'Sussex'),
+    ('warwickshire', 'Warwickshire'),
+    ('worcestershire', 'Worcestershire'),
+    ('yorkshire', 'Yorkshire'),
+]
+COMPETITION_CHOICES = [
+    ('cc1', 'County Champ Div 1'),
+    ('cc2', 'County Champ Div 2'),
+    ('t20', 'T20 Blast'),
+    ('oneday', 'One Day Cup'),
+    ('other', 'Other'),
+]
 
 # Create your models here.
 class Post(models.Model):
@@ -10,53 +57,9 @@ class Post(models.Model):
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name="blog_posts"
     )
-    STADIUM_CHOICES = [
-        ('racecourse', 'Racecourse Ground (Derbyshire)'),
-        ('riverside', 'Riverside (Durham)'),
-        ('chelmsford', 'Chelmsford (Essex)'),
-        ('sophia gardens', 'Sophia Gardens (Glamorgan)'),
-        ('nevil road', 'Nevil Road (Gloucestershire)'),
-        ('ageas bowl', 'Ageas Bowl (Hampshire)'),
-        ('canterbury', 'Canterbury (Kent)'),
-        ('old trafford', 'Old Trafford (Lancashire)'),
-        ('grace road', 'Grace Road (Leicestershire)'),
-        ('lords', 'Lords (Middlesex)'),
-        ('northampton', 'Northampton (Northamptonsire)'),
-        ('trent bridge', 'Trent Bridge (Nottinghamshire)'),
-        ('taunton', 'Taunton (Somerset)'),
-        ('oval', 'The Oval (Surrey)'),
-        ('hove', 'Hove (Sussex)'),
-        ('edgbaston', 'Edgbaston (Warwickshire)'),
-        ('new road', 'New Road (Worcestershire)'),
-        ('headingley', 'Headingley (Yorkshire)'),
-    ]
-    VISITORS_CHOICES = [
-        ('derbyshire', 'Derbyshire'),
-        ('durham', 'Durham'),
-        ('essex', 'Essex'),
-        ('glamorgan', 'Glamorgan'),
-        ('gloucestershire', 'Gloucestershire'),
-        ('hampshire', 'Hampshire'),
-        ('kent', 'Kent'),
-        ('lancashire', 'Lancashire'),
-        ('leicestershire', 'Leicestershire'),
-        ('middlesex', 'Middlesex'),
-        ('northamptonshire', 'Northamptonshire'),
-        ('nottinghamshire', 'Nottinghamshire'),
-        ('somerset', 'Somerset'),
-        ('surrey', 'Surrey'),
-        ('sussex', 'Sussex'),
-        ('warwickshire', 'Warwickshire'),
-        ('worcestershire', 'Worcestershire'),
-        ('yorkshire', 'Yorkshire'),
-    ]
-    COMPETITION_CHOICES = [
-        ('cc1', 'County Champ Div 1'),
-        ('cc2', 'County Champ Div 2'),
-        ('t20', 'T20 Blast'),
-        ('oneday', 'One Day Cup'),
-        ('other', 'Other'),
-    ]
+    stadium = models.CharField(max_length=50, choices=STADIUM_CHOICES, default='Lords')
+    visitor = models.CharField(max_length=50, choices=VISITORS_CHOICES, default='Surrey')
+    competition = models.CharField(max_length=20, choices=COMPETITION_CHOICES, default='County Champ Div 1')
     content = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     status = models.IntegerField(choices=STATUS, default=0)
