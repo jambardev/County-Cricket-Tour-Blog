7807d1ef8f642a870ce6f39fc2aa1660b94806fd
James Barlow <jamesbarlow1@hotmail.co.uk>
2024-07-03 11:05:49 +0000

    Import os correct

--- countychamp/settings.py
+++ countychamp/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
 import dj_database_url
 if os.path.isfile('env.py'):
     import env
@@ -26,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-ms@qat-raa!@ky*hkzx*8nl-%#ck&0ifuyl#-$w2-w15d-x7qj'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     "8000-jambardev-countycricket-8qmwuq0hhug.ws.codeinstitute-ide.net", 
