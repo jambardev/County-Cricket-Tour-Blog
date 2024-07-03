8f1936ae12e30509c2f4f82950b519458456a951
James Barlow <jamesbarlow1@hotmail.co.uk>
2024-07-03 11:24:01 +0000

    Secret key updated

--- countychamp/settings.py
+++ countychamp/settings.py
@@ -24,10 +24,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-ms@qat-raa!@ky*hkzx*8nl-%#ck&0ifuyl#-$w2-w15d-x7qj'
+SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = [
     "8000-jambardev-countycricket-8qmwuq0hhug.ws.codeinstitute-ide.net", 
