03d98e7a3fd972d2df5b4578589b5689dc0abc10
James Barlow <jamesbarlow1@hotmail.co.uk>
2024-07-10 07:24:06 +0000

    View for editing comments added

--- blog/views.py
+++ blog/views.py
@@ -45,3 +45,25 @@ def post_detail(request, slug):
             "comment_form": comment_form,
         }
     )
+
+# View allowing a user to edit their own comments
+
+def comment_edit(request, slug, comment_id):
+    
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Thanks, Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Sorry, Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
