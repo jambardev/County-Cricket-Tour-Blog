755d7db79d07b2fe0e8682fc628fd6ca136b7acc
James Barlow <jamesbarlow1@hotmail.co.uk>
2024-07-10 07:28:37 +0000

    Path for comment edit added to urls

--- blog/urls.py
+++ blog/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('about/', about_view, name='about'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
+    path('<slug:slug>/edit_comment/<int:comment_id>', views.comment_edit, name='comment_edit'),
 ]
