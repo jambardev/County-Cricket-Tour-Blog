e728bd71e4fca5e03ca6536d9a10a20f937c33bf
James Barlow <jamesbarlow1@hotmail.co.uk>
2024-07-12 21:49:34 +0000

    New post details added to forms.py

--- blog/forms.py
+++ blog/forms.py
@@ -1,8 +1,15 @@
+from .models import Post
 from .models import Comment
 from django import forms
 
+
+class PostForm(forms.ModelForm):
+    class Meta:
+        model = Post
+        fields = ['title', 'slug', 'stadium', 'visitor', 'competition', 'content', 'status']
 
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
         fields = ('body',)
+
