6d366e634673323cfc22774e26e22e66959ece74
James Barlow <jamesbarlow1@hotmail.co.uk>
2024-07-12 21:55:20 +0000

    add_post html template created

--- blog/urls.py
+++ blog/urls.py
@@ -1,10 +1,12 @@
 from . import views
 from django.urls import path
 from .views import about_view
+from . import views
 
 urlpatterns = [
     path('', views.PostList.as_view(), name='home'),
     path('about/', about_view, name='about'),
+    path('add_post/', views.add_post, name='add_post'),
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.edit_comment, name='edit_comment'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'),
