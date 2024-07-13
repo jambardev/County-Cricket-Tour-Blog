9142c0454bd0fc571cff7ea59e1836f4ae9e9fe3
James Barlow <jamesbarlow1@hotmail.co.uk>
2024-07-13 09:43:29 +0000

    urls.py updated for post deletion

--- blog/urls.py
+++ blog/urls.py
@@ -1,6 +1,6 @@
 from . import views
 from django.urls import path
-from .views import about_view
+from .views import about_view, delete_post
 from . import views
 
 urlpatterns = [
@@ -10,4 +10,5 @@ urlpatterns = [
     path('<slug:slug>/', views.post_detail, name='post_detail'),
     path('<slug:slug>/edit_comment/<int:comment_id>', views.edit_comment, name='edit_comment'),
     path('<slug:slug>/delete_comment/<int:comment_id>', views.delete_comment, name='delete_comment'),
+    path('<slug:slug>/delete_post/', delete_post, name='delete_post'),
 ]
